b8f4761356f4b342b564c20505e626883fe86f2a
SupKamatrip <raphael.ancelin@epsi.fr>
2023-02-22 18:44:02 +0100

    final commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+import time
+import sys
+
+# input va nous permettre de saisir le contenu de weathercity
+weathercity = input("Météo pour quelle  ville ? : ")
+
+# on récupère ci-dessus les données de la requetes web
+weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+weathercity+'&appid=9db93bdd1a0d2f47617d2c9158a903fa')
+url = ('http://api.openweathermap.org/data/2.5/weather?q='+weathercity+'&appid=9db93bdd1a0d2f47617d2c9158a903fa')
+                                                                # je vous laisse prendre ma clé d'API si vous voulez
+# Simulation d'un temps de chargement
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor  # yield retourne le générateur cursor ( au lieu de stocker on récupère l'info à la volée )
+                          # s'éxécute qu'une seule fois
+
+
+# liste en java de weather ( notre requete est stockée dans une liste)
+data = weather.json()
+
+for key, value in data.items():
+  print(key, value)
+
+
+# retourne les coordonnées gps de la station météo
+gps = data['coord']
+
+pays = data['sys']['country']
+
+humidite = data['main']['humidity']
+
+pression = data['main']['pressure']
+
+temp = data['main']['temp']
+
+venttemp = data['wind']['deg']
+
+ventsp = data['wind']['speed']
+
+description = data['weather'][0]['description']
+
+print(data)
+
+weatherprint = "\nActuellement en {}{}, à {} il fait {}°C: \n\n-Le taux d'humidité est de {}% et la pression " \
+               "atmosphérique est de {} hPa.\n-Le vent souffle à {} m/s \n\nLes coordonnées GPS de la station météo " \
+               "sont {}."
+
+# on fait appel à notre fonction précédemment définie
+spinner = spinning_cursor()
+
+# le _ permet d'ignorer l'itération, peut importe là ou ca en est la boucle est lancée
+for _ in range(25):
+
+    # nous permet d'écrire/afficher le logo de chargement
+    sys.stdout.write(next(spinner))
+
+    # Affiche immediatement le curseur plutot que d'attendre que le tampon soit plein
+    sys.stdout.flush()
+
+    # mets en pause 0.1 seconde a chaque itération dans la boucle for
+    time.sleep(0.1)
+
+    # efface le précédent caractère
+    sys.stdout.write('\b')
+
+# convertir les kelvin en celsius
+convert = int(temp - 273.15)
+
+print(weatherprint.format(*pays, weathercity, convert, humidite, pression, ventsp, gps))
